c367e993e24ca8802bda69e94f229932ed4e0a75
Ralu Ugochukwu <ralu2k3@gmail.com>
2023-03-07 10:41:28 +0000

    simplecalendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
+  resources :schedules
+  resources :habits do
+    resources :daily_trackings, only: [ :new, :create, :edit, :update, :destroy ]
+  end
 end
