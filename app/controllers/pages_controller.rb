20c1943caa20090bc3269cf4894eee6a11fe7bee
Shailan Sheibani <shailansheibani@yahoo.com>
2023-03-09 10:27:38 +0000

    setting routes page banner and footer done need styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,7 @@ class PagesController < ApplicationController
 
   def dashboard
   end
+
+  def setting
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   end
 
   get "/dashboard", to: "pages#dashboard"
+  get "/setting", to: "pages#setting"
 end
