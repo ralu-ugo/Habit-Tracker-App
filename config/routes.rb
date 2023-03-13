e391e9ec4e97bb818c449a39714ca1724fa88e01
Ralu Ugochukwu <ralu2k3@gmail.com>
2023-03-13 12:41:34 +0000

    styled calendars

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   get "/setting", to: "pages#setting"
 
-  get "/schedule_today", to: "pages#schedule_today", as: "schedule_today"
+  get "/schedule_today/:date", to: "pages#schedule_today", as: "schedule_today"
   get "/schedule", to: "pages#schedule_day_dynamic", as: "schedule_day_dynamic"
   get "/calendar", to: "pages#calendar", as: "calendar"
   get "/progress_page", to: "pages#progress_page", as: "progress_page"
