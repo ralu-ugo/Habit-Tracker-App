418e0f8a4357bc4dc4dd01a88c37e1645666b997
Sam Hillman <sam.hillman399@gmail.com>
2023-03-14 17:37:34 +0000

    js

---
+++ app/javascript/controllers/color_picker_controller.js
@@ -0,0 +1,49 @@
+import { Controller } from "@hotwired/stimulus"
+import ColorPicker from 'stimulus-color-picker'
+
+export default class extends ColorPicker {
+  connect() {
+    super.connect()
+    console.log('Do what you want here.')
+
+    // Pickr instance
+    this.picker
+  }
+
+  // Callback when the color is saved
+  onSave(color) {
+    super.onSave(color)
+  }
+
+  // You can override the components options with this getter.
+  // Here are the default options.
+  get componentOptions() {
+    return {
+      preview: true,
+      hue: true,
+
+      interaction: {
+        input: true,
+        clear: true,
+        save: true,
+      },
+    }
+  }
+
+  // You can override the swatches with this getter.
+  // Here are the default options.
+  get swatches() {
+    return [
+      '#A0AEC0',
+      '#F56565',
+      '#ED8936',
+      '#ECC94B',
+      '#48BB78',
+      '#38B2AC',
+      '#4299E1',
+      '#667EEA',
+      '#9F7AEA',
+      '#ED64A6',
+    ]
+  }
+}
