7770b9747b7c4a400e46cadfedbe1853274ec660
Sam Hillman <sam.hillman399@gmail.com>
2023-03-15 13:49:49 +0000

    js

--- app/javascript/controllers/checkbox_select_all_controller.js
+++ app/javascript/controllers/checkbox_select_all_controller.js
@@ -2,22 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-  console.log("hello111oooo");
+  console.log("hello1!!");
   }
 
   static targets = ["checkbox", "checkall"]
 
   checkAll() {
-console.log(this.checkboxTargets)
-
-    //  (this.checkallTarget.checked) {
+  if (this.checkallTarget.checked) {
+    this.checkboxTargets.forEach((checkboxx) => {
+      checkboxx.checked = true;
+    }) } else {
       this.checkboxTargets.forEach((checkboxx) => {
-        checkboxx.checked != checkboxx.checked;
-      // }) } else {
-      //   this.checkboxTargets.forEach((checkboxx) => {
-      //     checkboxx.checked = false;
-      });
+        checkboxx.checked = false;
+    });
     }
-
   }
-
+}
